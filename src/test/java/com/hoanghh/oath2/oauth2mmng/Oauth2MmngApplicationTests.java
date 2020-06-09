b222b1cdaa3a84860661b5523a5e86c49d56f9b1
hoanghh <hoanghh@nct.vn>
2020-06-09 16:51:34 +0700

    Init

---
+++ src/test/java/com/hoanghh/oath2/oauth2mmng/Oauth2MmngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghh.oath2.oauth2mmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2MmngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
